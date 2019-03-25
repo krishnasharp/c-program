55b8c7c97f41319c65e23ee1fa8a350da0f707f1
krishnasharp <48552410+krishnasharp@users.noreply.github.com>
2019-03-25 12:18:15 +0530

    Create 19.c

---
+++ player/set 2/19.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,count=0;
+	printf("Enter the Number:");
+	scanf("%d",&n);
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			for(j=2;j<i/2;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==0)
+			{
+				printf("%d",i);
+			}
+		}
+	}
+	return 0;
+  }
