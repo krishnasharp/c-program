73394e069646fb158f680372978ff1c297fd401d
krishnasharp <48552410+krishnasharp@users.noreply.github.com>
2019-03-25 11:25:10 +0530

    Create 11.c

---
+++ player/set 2/11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+ char str1[90]= "sunday", str2[90] = "saturday",str[90];
+    
+scanf("%s",&str);
+if(strcmp(str1, str)==0||strcmp(str2,str)==0)
+  {
+    printf("yes");
+  }
+   else 
+      {
+      printf("no");
+       }
+}
